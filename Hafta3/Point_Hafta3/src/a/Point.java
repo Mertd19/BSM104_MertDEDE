82c2e553926fe59bbd268b83084cc247b45f84f3
Mert Dede <85309911+Mertd19@users.noreply.github.com>
2022-03-12 00:42:49 +0300

    Add files via upload

---
+++ Hafta3/Point_Hafta3/src/a/Point.java
@@ -0,0 +1,15 @@
+package a;
+
+public class Point {
+	int x;
+	int y;
+	
+	public double distanceFromOrgin() {
+		return Math.sqrt(x*x + y*y);
+	}
+	
+	public void translate(int dx,int dy) {
+		x += dx;
+		y += dy;
+	}
+}
