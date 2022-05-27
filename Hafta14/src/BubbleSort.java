ab67e8bc378ca905475178eaed9be6912b5e53c8
Mert Dede <85309911+Mertd19@users.noreply.github.com>
2022-05-27 20:39:22 +0300

    Add files via upload

---
+++ Hafta14/src/BubbleSort.java
@@ -0,0 +1,36 @@
+
+public class BubbleSort {
+
+	void bubbleSort(int[] a) {
+		
+		for(int i = 0 ; i < a.length -1 ; i++) {
+			for(int j = 0;j < a.length - i - 1 ;j++) {
+				if(a[j] > a[j+1]) {
+					int gecici = a[j];
+					a[j] = a[j+1];
+					a[j+1] = gecici;
+				}
+			}
+		}
+	}
+	
+	void toString(int[] a) {
+		for(int i = 0; i < a.length;i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] a = {14,3,12,7,15,21,58,56,24};
+		
+		BubbleSort bs = new BubbleSort();
+		System.out.println("Sýrasýz Dizi : ");
+		bs.toString(a);
+		bs.bubbleSort(a);
+		System.out.println("Sýralý Dizi : ");
+		bs.toString(a);
+	}
+
+}
